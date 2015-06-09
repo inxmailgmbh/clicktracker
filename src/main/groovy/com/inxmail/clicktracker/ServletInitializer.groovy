b332473334c50791ec8492b6dc47d8ba225b9012
mrf <gh@nl.jtechsoft.de>
2015-06-09 21:33:26 +0200

    Created Spring Boot Application
    start with ./gradlew run

---
+++ src/main/groovy/com/inxmail/clicktracker/ServletInitializer.groovy
@@ -0,0 +1,13 @@
+package com.inxmail.clicktracker
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
+
+class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(ClicktrackerApplication)
+	}
+
+}
