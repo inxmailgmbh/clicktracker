d8f492c64c8906fb2bad9c3ff3bcc65c3245229a
cyphercrack <gh@nl.jtechsoft.de>
2015-06-10 20:26:48 +0200

    added a simple spock test

---
+++ src/test/groovy/com/clicktracker/ClicktrackerApplicationSpec.groovy
@@ -0,0 +1,16 @@
+package com.inxmail.clicktracker
+
+
+import spock.lang.Specification
+
+
+class ClicktrackerApplicationSpec extends Specification {
+
+	def "test the application"(){
+		when: 
+			// Test something
+			def exists = true
+		then:
+			exists
+	}
+}
